564977ad6a79d2dc4b7e9bb06555f961aff5dc0c
Alex Liu <avliu@buffalo.edu>
2020-04-01 17:51:14 +0800

    fixed monitor to have a correct running timer (albeit hacky fix)

--- Client_2/src/Monitor.java
+++ Client_2/src/Monitor.java
@@ -16,17 +16,23 @@ public class Monitor extends Service {
         // ask for user input
         String[] request_values = get_user_request_values();
         // in milliseconds
-        int monitor_time = Integer.parseInt(request_values[1]);
+        int monitor_period = Integer.parseInt(request_values[1]);
         int monitor_request_id = runner.get_request_id();
+        long monitor_start = System.currentTimeMillis();
 
         Map<String, Object> monitor_reply = send_and_receive(request_values);
         List<Byte> update_bytes;
 
         if ((int) monitor_reply.get("status") == Constants.SUCCESSFUL_STATUS_ID) {
             System.out.println("request success");
-            runner.socket.setSoTimeout(monitor_time);
+            runner.socket.setSoTimeout(monitor_period);
             try {
                 while(true) {
+                    long current_time = System.currentTimeMillis();
+                    if (current_time - monitor_start >= monitor_period) {
+                        throw new SocketTimeoutException();
+                    }
+                    runner.socket.setSoTimeout((int) (monitor_period - (current_time - monitor_start)));
                     try {
                         update_bytes = Util.receive_message(monitor_request_id, runner);
                         // we know that service id is not needed here
@@ -42,7 +48,6 @@ public class Monitor extends Service {
             catch (SocketTimeoutException t) {
                 System.out.println("Timed out; Done receiving updates");
             }
-
         }
         else {
             System.out.println("error: " + monitor_reply.get("message"));
