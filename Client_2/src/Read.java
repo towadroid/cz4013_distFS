c4824295ad1109b75e09050f3e6e71f6fbe9225d
Alex Liu <avliu@buffalo.edu>
2020-04-02 18:43:28 +0800

    Read

--- Client_2/src/Read.java
+++ Client_2/src/Read.java
@@ -13,10 +13,29 @@ public class Read extends  Service {
 
         //ask for user input
         String[] request_values = get_user_request_values();
+        String pathname = request_values[0];
+        int offset = Integer.parseInt(request_values[1]);
+        int byte_count = Integer.parseInt(request_values[2]);
 
-        Map<String, Object> reply = send_and_receive(request_values);
-        int file_content_size = ((String) reply.get("content")).length();
-        System.out.println("reply received");
+        if (!runner.cache.containsKey(pathname)) {
+            runner.cache.put(pathname, new CacheObject(pathname, runner));
+        }
+        CacheObject cache_object = runner.cache.get(pathname);
+
+        boolean cached = cache_object.has_range(offset, byte_count);
+        boolean local_fresh = cache_object.validate_local();
+        boolean server_fresh = true;
+        if (!local_fresh) {
+            server_fresh = cache_object.validate_server(runner);
+        }
+
+        if (!cached || cached && !local_fresh && !server_fresh) {
+            Map<String, Object> reply = send_and_receive(request_values);
+            cache_object.set_cache(offset, byte_count, (String) reply.get("content"));
+        }
+
+        String content = cache_object.get_cache(offset, byte_count);
+        System.out.println(content);
 
     }
 
